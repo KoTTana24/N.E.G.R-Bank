64f57c08485b6f58b433ad52b11dd6bca1ee687a
Alexander <supergeniy337@gmail.com>
2026-06-04 17:14:46 +0700

    Update game_data.py

--- game_data.py
+++ game_data.py
@@ -3,28 +3,63 @@ import os
 
 DATA_FILE = "player_data.json"
 
+
 class GameData:
     balance = 0
     level = 1
 
+    brokerage_balance = 5000
+
+    stocks = {}
+
+    stock_prices = {
+        "negr_bank": 1000,
+        "mine": 3000
+    }
+
     @classmethod
     def load(cls):
-        if os.path.exists(DATA_FILE):
-            try:
-                with open(DATA_FILE, "r", encoding="utf-8") as f:
-                    data = json.load(f)
-                    cls.balance = data.get("balance", 0)
-                    cls.level = data.get("level", 1)
-            except Exception:
-                cls.balance = 0
-                cls.level = 1
-        else:
-            cls.save()  # создаём файл, если нет
+        if not os.path.exists(DATA_FILE):
+            cls.save()
+            return
+
+        try:
+            with open(DATA_FILE, "r", encoding="utf-8") as file:
+                data = json.load(file)
+
+            cls.balance = data.get("balance", 0)
+            cls.level = data.get("level", 1)
+
+            cls.brokerage_balance = data.get(
+                "brokerage_balance",
+                5000
+            )
+
+            cls.stocks = data.get("stocks", {})
+
+            cls.stock_prices = data.get(
+                "stock_prices",
+                {
+                    "negr_bank": 1000,
+                    "mine": 3000
+                }
+            )
+
+        except Exception:
+            cls.save()
 
     @classmethod
     def save(cls):
-        with open(DATA_FILE, "w", encoding="utf-8") as f:
-            json.dump({
-                "balance": cls.balance,
-                "level": cls.level
-            }, f)
+        with open(DATA_FILE, "w", encoding="utf-8") as file:
+            json.dump(
+                {
+                    "balance": cls.balance,
+                    "level": cls.level,
+                    "brokerage_balance": cls.brokerage_balance,
+                    "stocks": cls.stocks,
+                    "stock_prices": cls.stock_prices
+                },
+                file,
+                indent=4,
+                ensure_ascii=False
+            )
